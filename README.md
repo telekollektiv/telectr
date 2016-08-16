45d2fcaaaf918298faadd452b8ec24129d6e7714
kpcyrd <git@rxv.cc>
2016-08-17 01:02:54 +0200

    Add rust server

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# telecounter
+# telectr
 A web based (+/-1) increase/decrease (person) counter which collects counts from multiple interfaces and summarizes them in one place (somehow)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,62 @@
+extern crate redis;
+extern crate ws;
+
+use redis::Client;
+use ws::{ listen, Handler, Sender, Result, Message, CloseCode };
+
+struct Server {
+    out: Sender,
+    sock: Client,
+}
+
+impl Handler for Server {
+    fn on_message(&mut self, msg: Message) -> Result<()> {
+        let txt = msg.as_text().unwrap();
+        let sock = self.sock.get_connection().unwrap();
+
+        let key = &txt[1..];
+
+        let value = match txt.chars().next().unwrap() {
+            '+' => {
+                println!("inc {}", key);
+                match redis::cmd("INCR").arg(key).query(&sock) {
+                    Ok(value) => { value },
+                    Err(_) => { -1 },
+                }
+            }
+            '-' => {
+                println!("dec {}", key);
+                match redis::cmd("DECR").arg(key).query(&sock) {
+                    Ok(value) => { value },
+                    Err(_) => { -1 },
+                }
+            }
+            _ => {
+                match redis::cmd("GET").arg(key).query(&sock) {
+                    Ok(value) => { value },
+                    Err(_) => { -1 },
+                }
+            }
+        };
+
+        self.out.send(Message::from(value.to_string()))
+    }
+
+    fn on_close(&mut self, code: CloseCode, reason: &str) {
+        match code {
+            CloseCode::Normal   => println!("the client is done"),
+            CloseCode::Away     => println!("the client is leaving"),
+            _ => println!("client encountered an error {}", reason),
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let addr = "127.0.0.1:3012";
+
+    println!("[*] starting ({})...", addr);
+    listen(addr, |out| Server {
+        out: out,
+        sock: Client::open("redis://127.0.0.1/").unwrap(),
+    }).unwrap()
 }
